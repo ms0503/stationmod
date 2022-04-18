6ffcfbdda782ce35853893a4e97553b0ed1af3d6
Sora Tonami <ms0503@outlook.com>
2022-04-18 22:19:16 +0900

    nebula-release-pluginを削除。

--- src/main/java/ms/sora/stationmod/ModInfo.java
+++ src/main/java/ms/sora/stationmod/ModInfo.java
@@ -3,5 +3,5 @@ package ms.sora.stationmod;
 public class ModInfo {
     public static final String MOD_ID = "stationmod";
     public static final String MOD_NAME = "Station MOD / 駅MOD";
-    public static final String MOD_VERSION = "0.1.0-dev.0.uncommitted";
+    public static final String MOD_VERSION = "0.1.0";
 }
